1578d608abc4ac754c52c8dacebe1e91fca5bc32
abhijeetpnwr <abhijeetpnwr@gmail.com>
2017-06-08 00:48:04 +0530

    test

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 import os
 
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
+from stories/wsgi import application
+
 def application(environ, start_response):
 
     ctype = 'text/plain'
